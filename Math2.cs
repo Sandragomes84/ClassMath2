31421a264027826983089748a4d7ad728fba4725
Sandra Gomes <sandrinhagomes84@gmail.com>
2022-09-14 18:47:14 +0100

    Classe Math2

---
+++ Math2.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Math2
+{
+    public static class Math2
+    {
+
+
+
+        public static int Min(int x, int y)
+        {
+            if (x < y)
+            {
+                return x;
+            }
+            else  
+            {
+                return y;
+            }
+           
+
+        }
+
+        public static int Max(int x, int y)
+        {
+            if (x > y)
+            {
+                return x;
+            }
+            else
+            {
+                return y;
+            }
+        }
+
+        public static bool AreEqual(int x, int y)
+        {
+            if (x == y)
+            {
+                return true;
+            }
+            else { return false; }
+
+        }
+
+        public static int Sum(int x, int y)
+        {
+
+            return x + y;
+
+        }
+
+        public static void Swap(int x, int y)
+        {
+                int aux;
+
+                aux = y;
+                y = x;
+                x = aux;
+
+            Console.WriteLine("Após troca de valores: x = " + x + " e y = " + y);
+            
+
+
+
+        }
+
+
+        
+    }
+}    
+ 
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Class_Math2;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Intrinsics.X86;
+
+int x = 10;
+int y = 20;
+
+int min = Math2.Min(x, y);
+int max = Math2.Max(x, y);
+bool equal = Math2.AreEqual(x, y);
+int sum = Math2.Sum(x, y);
+
+
+
+Console.WriteLine(" O valor inteiro mínimo é: " + min);
+Console.WriteLine(" O valor inteiro máximo é: " + max);
+Console.WriteLine(" Os valores são iguais ? " + equal);
+Console.WriteLine(" O valor da soma é: " + sum);
+Math2.Swap(x, y);
+
+
+Console.Read();
